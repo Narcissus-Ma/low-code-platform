4ca4a4a559ce70f56d50c665a16a7a6dc6761bd1
Narcissus-Ma <103873240+Narcissus-Ma@users.noreply.github.com>
2025-01-12 20:15:31 +0800

    Updates

--- assets/debug-72dbe89e.js
+++
@@ -1,1 +0,0 @@
-import{bE as t,r as o}from"./index-98220b7f.js";const u=t("env",()=>{const e=o(!0);function r(){e.value=!e.value}return{debug:e,toggle:r}});export{u};
---
+++ assets/debug-7478529d.js
@@ -0,0 +1,1 @@
+import{bE as t,r as o}from"./index-b57493aa.js";const u=t("env",()=>{const e=o(!0);function r(){e.value=!e.value}return{debug:e,toggle:r}});export{u};
