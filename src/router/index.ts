2ddb87168d11d43b3170332c8cb016a1f17b41f6
Narcissus-Ma <103873240+Narcissus-Ma@users.noreply.github.com>
2025-01-13 12:38:50 +0800

    fix: 动态导入问题暂时修复

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,17 @@
 import { createRouter, createWebHashHistory  } from 'vue-router'
 
 // import AppView from '../views/AppView.vue'
+import DataSourceView from '../views/DataSourceView.vue'
+import DataSourceContent from '../views/DataSourceContent/DataSourceContent.vue'
+// import PageLayoutView from '../views/PageLayoutView.vue'
 
+// todo 动态引入，部署到线上会报错，问题尚不明确
 const router = createRouter({
   history: createWebHashHistory (import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      redirect: '/app/actions'
+      redirect: '/app/layout'
     },
     {
       path: '/app',
@@ -17,11 +21,13 @@ const router = createRouter({
         {
           path: 'dataSource',
           name: 'dataSource',
-          component: () => import('../views/DataSourceView.vue'),
+          // component: () => import('../views/DataSourceView.vue'),
+          component: DataSourceView,
           children: [
             {
               path: ':id',
-              component: () => import('../views/DataSourceContent/DataSourceContent.vue')
+              // component: () => import('../views/DataSourceContent/DataSourceContent.vue')
+              component: DataSourceContent
             },
             {
               path: '',
@@ -33,6 +39,7 @@ const router = createRouter({
           path: 'layout',
           name: 'layout',
           component: () => import('../views/PageLayoutView.vue')
+          // component: PageLayoutView
         },
         {
           path: 'actions',
